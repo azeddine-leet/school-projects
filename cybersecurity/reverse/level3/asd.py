2c24cab37e5e9bcca2f9a86405468ed0d36517f2
Azeddine Elmakkaoui <azeddine.elmakkaoui@gmail.com>
2024-01-04 13:42:25 +0100

    angr solution & installation guide

---
+++ cybersecurity/reverse/level3/asd.py
@@ -0,0 +1,7 @@
+import angr
+
+proj = angr.Project('level3')
+simgr = proj.factory.simgr()
+simgr.explore(find=lambda s: b"Good job." in s.posix.dumps(1))
+s = simgr.found[0]
+print(s.posix.dumps(0))
